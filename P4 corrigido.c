1daa818db4161a22ff9c23b9f908e9b0390ae12b
otaviozumpano <37805934+otaviozumpano@users.noreply.github.com>
2018-07-01 09:44:15 -0300

    Add files via upload

---
+++ P4 corrigido.c
@@ -0,0 +1,23 @@
+//Otavio Zumpano Cunha Rodrigues
+//11721EEl016
+
+#include <stdio.h>
+
+int main ()
+{
+	int i=0, cum=0;
+	char num[300];
+	printf("Digite a string: \n");
+	fgets(num,300,stdin);
+	while(num[i])
+	{
+		if(num[i]>=48 && num[i]<=57)
+		{
+			num[i]=num[i]-'0';
+			cum = cum*10+num[i];
+		}
+		i++;
+	}
+	printf("Nova string: %d", cum);
+	return 0;
+}
